17dcce2e399efc2db653fb709266f5dcbbe806bb
nghiahsgs <nghiahsgs@gmail.com>
2021-07-26 16:02:42 +0700

    make it better

---
+++ constants.js
@@ -0,0 +1,3 @@
+const CHROME_ID = 'lap_chrome_trunk'
+const BASE_URL = 'http://localhost:3000'
+// const BASE_URL = 'https://sse.nghientrang.com'
---
+++ content.js
@@ -0,0 +1,9 @@
+$(document).ready(()=>{
+  console.log("on ready")
+  document.querySelector('[data-pagelet="RightRail"] div span div').style.display='none';
+})
+
+$(document).load(()=>{
+  console.log("on load")
+  document.querySelector('[data-pagelet="RightRail"] div span div').style.display='none';
+})
